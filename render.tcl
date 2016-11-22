9a61b39f70b3104aed9e721fe1a7d7db6e08c064
Ruggero Cortini <ruggero.cortini@crg.eu>
2016-11-22 11:21:47 +0100

    improved hq_render: doesn't ask for x and y

--- render.tcl
+++ render.tcl
@@ -1,3 +1,6 @@
-proc hq_render {name x y} {
+proc hq_render {name} {
+  set size [display get size]
+  set x [lindex $size 0]
+  set y [lindex $size 1]
   render Tachyon "$name.dat" "/usr/bin/tachyon" -aasamples 12 -res [expr $x*1.5] [expr $y*1.5] -fullshade %s -format TARGA -o %s.tga
 }
